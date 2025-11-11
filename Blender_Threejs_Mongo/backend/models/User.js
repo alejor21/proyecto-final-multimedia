36d58750b16165ca73303cd891313f61cb01eb09
Julian-Escobar22 <julian.escobarf@campusucc.edu.co>
2025-11-11 16:37:42 -0500

    subiendo cambios JWT

--- Blender_Threejs_Mongo/backend/models/User.js
+++ Blender_Threejs_Mongo/backend/models/User.js
@@ -1,12 +1,8 @@
-// backend/models/User.js
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-    {
-        email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-        password: { type: String, required: true }
-    },
-    { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  email:    { type: String, required: true, unique: true, lowercase: true, trim: true },
+  password: { type: String, required: true } 
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
