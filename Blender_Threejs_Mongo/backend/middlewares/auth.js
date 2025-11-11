9843044215d5b1b44501707145b8b5a854125151
Julian-Escobar22 <julian.escobarf@campusucc.edu.co>
2025-11-11 17:32:05 -0500

    subiendo Correción minima JWT

--- Blender_Threejs_Mongo/backend/middlewares/auth.js
+++ Blender_Threejs_Mongo/backend/middlewares/auth.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 exports.authRequired = (req, res, next) => {
-  try {
-    const header = req.headers.authorization || '';
-    const [, token] = header.split(' ');
-    if (!token) return res.status(401).json({ error: 'Token requerido' });
+    try {
+        const header = req.headers.authorization || '';
+        const [, token] = header.split(' ');
+        if (!token) return res.status(401).json({ error: 'Token requerido' });
 
-    const payload = jwt.verify(token, process.env.JWT_SECRET); 
-    req.user = payload;
-    next();
-  } catch (e) {
-    return res.status(401).json({ error: 'Token inválido o expirado' });
-  }
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = payload;
+        next();
+    } catch (e) {
+        return res.status(401).json({ error: 'Token inválido o expirado' });
+    }
 };
